b5f758932dfccdc38ac2d0c965b54c2646161537
opilane <opilane@HP-CZC321706M>
2025-11-13 17:22:53 +0200

    Create

--- TestProject2/LogIn_Test.cs
+++ TestProject2/LogIn_Test.cs
@@ -3,7 +3,7 @@ using OpenQA.Selenium.Firefox;
 
 namespace TestProject2
 {
-    public class Admin_Test
+    public class LogIn_Test
     {
         private static IWebDriver Start()
         {
@@ -88,6 +88,7 @@ namespace TestProject2
             Thread.Sleep(500);
  
         }
+        //login poss
         [Fact]
         public void LogOut()
         {
@@ -107,6 +108,71 @@ namespace TestProject2
             IWebElement idOfLogOut = driver.FindElement(By.Id("test_logOut"));
             idOfLogOut.Click();
             Thread.Sleep(500);
+        }
+        //Create pos
+        [Fact]
+        public void CreatePost()
+        {
+            IWebDriver driver = Start();
+            IWebElement idOfRealLogIN = driver.FindElement(By.Id("test_indexLogIn"));
+            idOfRealLogIN.Click();
+            Thread.Sleep(500);
+
+            GetLogInData(driver);
+            Thread.Sleep(500);
+
+            IWebElement idOfRealCreateNewPost = driver.FindElement(By.Id("test_Create_Post"));
+            idOfRealCreateNewPost.Click();
+            Thread.Sleep(500);
+
+            GetCreateData(driver);
+
+            //Image korda
+            //IWebElement idOfRealCreateButton = driver.FindElement(By.Id("test_Create_Button"));
+            //idOfRealCreateButton.Click();
+            //Thread.Sleep(500);
+
+
+        }
+        private static void GetCreateData(IWebDriver driver)
+        {
+            IWebElement idOfCreateName = driver.FindElement(By.Id("test_Create_Name"));
+            idOfCreateName.Clear();
+            idOfCreateName.SendKeys("Mihkel");
+            Thread.Sleep(500);
+
+            IWebElement idOfCreateSpecies = driver.FindElement(By.Id("test_Create_Species"));
+            idOfCreateSpecies.Clear();
+            idOfCreateSpecies.SendKeys("krants");
+            Thread.Sleep(500);
+
+            IWebElement idOfCreateAge = driver.FindElement(By.Id("test_Create_Age"));
+            idOfCreateAge.Clear();
+            idOfCreateAge.SendKeys("15");
+            Thread.Sleep(500);
+
+            IWebElement idOfCreateGender = driver.FindElement(By.Id("test_Create_Gender"));
+            idOfCreateGender.Clear();
+            idOfCreateGender.SendKeys("male");
+            Thread.Sleep(500);
+
+            IWebElement idOfCreateTitle = driver.FindElement(By.Id("test_Create_Title"));
+            idOfCreateTitle.Clear();
+            idOfCreateTitle.SendKeys("Krants kass");
+            Thread.Sleep(500);
+
+            IWebElement idOfCreateDescription = driver.FindElement(By.Id("test_Create_Description"));
+            idOfCreateDescription.Clear();
+            idOfCreateDescription.SendKeys("bla bla cat");
+            Thread.Sleep(500);
+
+            //Image
+            //IWebElement idOfCreateImage = driver.FindElement(By.Id("test_Create_Image"));
+            //idOfCreateImage.Clear();
+            //idOfCreateImage.SendKeys("TestPassword");
+            //Thread.Sleep(500);
+
+
         }
 
     }
