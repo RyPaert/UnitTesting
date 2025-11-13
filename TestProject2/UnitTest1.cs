89333b441bf3c149b00bc09653180b567c8ec5ff
Ry <thomashenrypaert@gmail.com>
2025-11-13 14:23:52 +0200

    fuck

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -8,42 +8,14 @@ namespace TestProject2
         [Fact]
         public void Test1()
         {
-            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\Testing\\FirefoxPortable\\App\\Firefox64\\firefox.exe";
+            string binaryLocation = "";
             FirefoxOptions options = new FirefoxOptions();
             options.BinaryLocation = binaryLocation;
 
-            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\Testing\\TestProject2\\bin\\Debug\\net8.0", options);
-            driver.Url = "https://localhost:44305/";
-
-            IWebElement idOfRegisterIndex = driver.FindElement(By.Id("test_register_index"));
-            idOfRegisterIndex.Click();
+            IWebDriver driver = new FirefoxDriver("", options);
+            driver.Url = "https://github.com/RyPaert";
 
             Thread.Sleep(1000);
-
-            InsertRegistrationData(driver);
-
-            IWebElement registerAccount = driver.FindElement(By.Id("test_registerAccountButton"));
-            registerAccount.Click();
-
-        }
-
-        private void InsertRegistrationData(IWebDriver driver)
-        {
-            IWebElement idofName = driver.FindElement(By.Id("test_Username"));
-            idofName.SendKeys("Jane Doe");
-            Thread.Sleep(500);
-
-            IWebElement idofEmail = driver.FindElement(By.Id("test_Email"));
-            idofEmail.SendKeys("Jane.Doe@gmail.com");
-            Thread.Sleep(500);
-
-            IWebElement idofPassword = driver.FindElement(By.Id("test_Password"));
-            idofPassword.SendKeys("JaneDoe1234");
-            Thread.Sleep(500);
-
-            IWebElement idofConfirmPassword = driver.FindElement(By.Id("test_ConfirmPassword"));
-            idofConfirmPassword.SendKeys("JaneDoe1234");
-            Thread.Sleep(500);
 
         }
     }
