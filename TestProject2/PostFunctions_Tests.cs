4a777f20185d467b86c72d63f0108911186a6354
Ry <thomashenrypaert@gmail.com>
2025-11-27 08:38:41 +0200

    setup for post function tests

---
+++ TestProject2/PostFunctions_Tests.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2
+{
+	[TestCaseOrderer(
+	ordererTypeName: "TestProject2.PriorityOrderer",
+	ordererAssemblyName: "TestProject2")]
+	public class PostFunctions_Tests
+	{
+		private static IWebDriver Start()
+		{
+			string binaryLocation = "C:\\Users\\rylei\\Downloads\\FirefoxPortable\\App\\Firefox64\\firefox.exe";
+			FirefoxOptions options = new FirefoxOptions();
+			options.BinaryLocation = binaryLocation;
+
+			IWebDriver driver = new FirefoxDriver("C:\\Users\\rylei\\source\\repos\\UnitTesting\\TestProject2\\driver\\geckodriver.exe", options);
+			driver.Url = "https://localhost:7260/";
+			return driver;
+		}
+
+
+	}
+}
